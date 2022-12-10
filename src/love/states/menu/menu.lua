efce9b65629f69d743f0e1274bf9a684f0676517
Bradley <bradsbluetab@gmail.com>
2022-12-10 18:38:02 -0500

    cock

--- src/love/states/menu/menu.lua
+++ src/love/states/menu/menu.lua
@@ -42,26 +42,10 @@ return {
 		logo:setFrames(paths.getSparrowFrames("logoBumpin"))
    		logo:addAnimByPrefix("bump", "logo bumpin", 24)
 		
-		girlfriendTitle = sprite(-25, -275)
-		girlfriendTitle:setFrames(paths.getSparrowFrames("menu/girlfriend-title"))
-		girlfriendTitle:addAnimByIndices("danceLeft", "gfDance",
-                    {30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}, 24)
-
-		girlfriendTitle:addAnimByIndices("danceRight", "gfDance", {
-					15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29}, 24)
-
 		logo:animate("bumpin", false)
-		girlfriendTitle:animate("danceRight", false)
 
 		if love.system.getOS() == "OS X" and gamejoltLogin["useGamejolt"] then
 			gamejolt.giveTrophy(175141)
-		end
-
-		whiteRectangles = {}
-		for i = 1, 15 do
-			table.insert(whiteRectangles, graphics.newImage(love.graphics.newImage(graphics.imagePath("menu/whiteRectangle"))))
-			whiteRectangles[i].x = -780 + 100*i
-			whiteRectangles[i].y = -1000
 		end
 
 		songNum = 0
@@ -74,21 +58,11 @@ return {
 		updatePres("Press Enter", "In the Menu")
 	end,
 	onBeat = function(self, n)
-		danceRight = not danceRight
-		if girlfriendTitle then 
-			if n % 2 == 0 then
-				girlfriendTitle:animate("danceRight", false)
-			else
-				girlfriendTitle:animate("danceLeft", false)
-			end 
-		end
-
 		--if logo then logo:animate("anim", false) end
 		if logo then logo:animate("bump", false) end
 	end,
 
 	update = function(self, dt)
-		girlfriendTitle:update(dt)
 		logo:update(dt)
 
 		music[1]:updateBeat()
@@ -99,19 +73,10 @@ return {
 				if not changingMenu then
 					audio.playSound(confirmSound)
 					changingMenu = true
-					for i = 1, 15 do
-						Timer.tween(0.5 + 0.1 + 0.03*i, whiteRectangles[i], {y = 0}, "linear",
-							function()
-								if i == 15 then
-									graphics.fadeOut(0.2, function()
-										Gamestate.switch(menuSelect)
-										status.setLoading(false)
-									end)
-									
-								end
-							end
-						)
-					end
+					graphics.fadeOut(0.2, function()
+						Gamestate.switch(menuSelect)
+						status.setLoading(false)
+					end)
 				end
 			elseif input:pressed("back") then
 				audio.playSound(selectSound)
@@ -138,13 +103,9 @@ return {
 				love.graphics.push()
 					love.graphics.scale(0.9, 0.9)
 					--love.graphics.translate(menuDetails.girlfriendTitle.x, menuDetails.girlfriendTitle.y)
-					girlfriendTitle:draw()
 				love.graphics.pop()
 				love.graphics.push()
 					graphics.setColor(0, 0, 0, 0.9)
-					for i = 1, 15 do
-						whiteRectangles[i]:draw()
-					end
 					graphics.setColor(1, 1, 1)
 				love.graphics.pop()
 			love.graphics.pop()
